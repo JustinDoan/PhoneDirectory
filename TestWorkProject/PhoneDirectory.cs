d6844c0d496f173dc7d15b4623864347b72773f3
Justin Doan <JustinDoan@mail.com>
2018-11-13 16:39:32 -0500

    Init Commit

---
+++ TestWorkProject/PhoneDirectory.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWorkProject
+{
+    enum PhoneNumberStatus
+    {
+        Nonexisitant = 0,
+        Available = 1,
+        Unavailable = 2
+    }
+    class PhoneDirectory
+    {
+
+        List<PhoneNumber> PhoneNumbers;
+
+        public PhoneDirectory(List<PhoneNumber> phoneNumbers)
+        {
+            PhoneNumbers = phoneNumbers;
+        }
+
+
+        public PhoneNumber Get()
+        {
+            //Get the first available number
+            try
+            {
+                PhoneNumber availableNumber = PhoneNumbers.Where(x => x.Status == PhoneNumberStatus.Available).First();
+                return availableNumber;
+            }
+            catch (InvalidOperationException)
+            {
+                //There were no available numbers to get
+                return null;
+                throw new InvalidOperationException($"There were no available numbers in the Phone Directory.");
+            }
+
+        }
+        public PhoneNumberStatus Check(int number)
+        {
+            try
+            {
+                PhoneNumber numberToCheck = PhoneNumbers.Where(x => x.Number == number).First();
+                return numberToCheck.Status;
+            }
+            catch (InvalidOperationException)
+            {
+                //that number didn't exist
+                return PhoneNumberStatus.Nonexisitant;
+                throw new InvalidOperationException($"{number} did not exist in the Phone Directory.");
+            }
+        }
+        public Boolean Release(int number)
+        {
+            try
+            {
+                PhoneNumber numberToCheck = PhoneNumbers.Where(x => x.Number == number).First();
+                numberToCheck.Release();
+                if (numberToCheck.Status != PhoneNumberStatus.Available) { return false; }
+                return true;
+            }
+            catch (InvalidOperationException)
+            {
+                //that number didn't exist
+                return false;
+                throw new InvalidOperationException($"{number} did not exist in the Phone Directory.");
+            }
+
+
+
+
+        }
+    }
+    class PhoneNumber
+    {
+
+        public PhoneNumberStatus Status { get; set; }
+        public int Number { get; set; }
+
+        public PhoneNumber(int number)
+        {
+            Number = number;
+            Status = PhoneNumberStatus.Available;
+        }
+
+        public void Release()
+        {
+            //First check if the number is not available
+            if (Status == PhoneNumberStatus.Unavailable)
+            {
+                Status = PhoneNumberStatus.Available;
+            }
+        }
+
+
+
+    }
+}
--- TestWorkProject/Program.cs
+++ TestWorkProject/Program.cs
@@ -10,6 +10,46 @@ namespace TestWorkProject
     {
         static void Main(string[] args)
         {
+            List<PhoneNumber> phoneNumbers = new List<PhoneNumber>() {new PhoneNumber(1), new PhoneNumber(2), new PhoneNumber(3)};
+            PhoneDirectory directory = new PhoneDirectory(phoneNumbers);
+            PhoneNumber phoneNumber = directory.Get();
+            Console.WriteLine($"{phoneNumber.Number} was selected, it's status is {phoneNumber.Status}");
+            phoneNumber.Status = PhoneNumberStatus.Unavailable;
+            phoneNumber = directory.Get();
+            Console.WriteLine($"{phoneNumber.Number} was selected, it's status is {phoneNumber.Status}");
+            phoneNumber.Status = PhoneNumberStatus.Unavailable;
+            phoneNumber = directory.Get();
+            Console.WriteLine($"{phoneNumber.Number} was selected, it's status is {phoneNumber.Status}");
+            phoneNumber.Status = PhoneNumberStatus.Unavailable;
+            phoneNumber = directory.Get();
+            if (phoneNumber == null)
+            {
+                Console.WriteLine("No Available Numbers");
+            }
+            else
+            {
+                Console.WriteLine($"{phoneNumber.Number} was selected, it's status is {phoneNumber.Status}");
+            }
+            Console.WriteLine(directory.Check(1));
+            Console.WriteLine(directory.Check(2));
+            Console.WriteLine(directory.Check(3));
+            Console.WriteLine(directory.Check(4));
+
+
+            directory.Release(1);
+            directory.Release(2);
+            directory.Release(3);
+            directory.Release(4);
+
+            phoneNumber = directory.Get();
+            Console.WriteLine($"{phoneNumber.Number} was selected, it's status is {phoneNumber.Status}");
+            phoneNumber.Status = PhoneNumberStatus.Unavailable;
+            phoneNumber = directory.Get();
+            Console.WriteLine($"{phoneNumber.Number} was selected, it's status is {phoneNumber.Status}");
+            phoneNumber.Status = PhoneNumberStatus.Unavailable;
+            phoneNumber = directory.Get();
+            Console.WriteLine($"{phoneNumber.Number} was selected, it's status is {phoneNumber.Status}");
+            phoneNumber.Status = PhoneNumberStatus.Unavailable;
         }
     }
 }
